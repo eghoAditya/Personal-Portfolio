9bb6213a01f71942593606b0c920e50950f00f92
Aditya <jackuserme8881@gmail.com>
2024-11-15 22:59:08 +0530

    Created contact form and integrated API from emailjs to receive emails

--- main.js
+++ main.js
@@ -31,4 +31,50 @@ var quoteSwiper = new Swiper(".qoutes_container", {
     },
     mousewheel: true,
     keyboard: true,
-  });
+});
+  
+/*-- CONTACT FORM--*/
+const contactForm = document.getElementById('contact-form'),
+  contactName = document.getElementById('contact-name'),
+  contactEmail = document.getElementById('contact-email'),
+  Message = document.getElementById('message'),
+  contactMessage = document.getElementById('contact-message');
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  //cheching if the field has a value
+  if (
+    contactName.value == '' ||
+    contactEmail.value == '' ||
+    Message.value == ''
+  ){
+    // add and remove color
+    contactMessage.classList.remove('color-light');
+    contactMessage.classList.add('color-dark');
+
+    // show message
+    contactMessage.textContent = 'Write all the input fields'; 
+  } else {
+    // serviceID - templateID - #form - publickey
+    emailjs
+      .sendForm(
+      'service_j6fyeo8',
+      'template_ys0sgir',
+      '#contact-form',
+      'kYEWVBteDC6g7JQ20'
+    )
+      .then(() => {
+        //show message and add colour
+        contactMessage.classList.add('color-light');
+        console.log(contactMessage);
+        contactMessage.textContent = 'Message sent ✔';
+
+        //remove message after 5 secs
+        setTimeout(() => {
+          contactMessage.textContent = '';
+        }, 5000);
+      });
+  }
+};
+contactForm.addEventListener('submit', sendEmail);
